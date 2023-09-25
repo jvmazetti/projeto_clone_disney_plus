38ade3eefbf92bd308eed16d0e6d9f4cba077eb5
João Victor Mazetti Borges <jvmazettib@gmail.com>
2023-09-25 17:07:58 -0300

    fazendo algumas modificações

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
-	const button = document.querySelectorAll('[data-tab-button]');
+	const buttons = document.querySelectorAll('[data-tab-button]');
 	const questions = document.querySelectorAll('[data-faq-question]')
 
 
-	for(let i = 0; i < button.lenght; i++){
-		button[i].addEventListener('click', function(botao){
+	for(let i = 0; i < buttons.lenght; i++){
+		buttons[i].addEventListener('click', function(botao){
 			const abaAlvo = botao.target.dataset.tabButton;
-			const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+			const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
 			escondeTodasAbas();
-			aba.classList.add('shows__list--is-active');
+			abaAlvo.classList.add('shows__list--is-active');
 			removeBotaoAtivo();
 			botao.target.classList.add('shows__tabs__button--is-active')
 		})
@@ -28,9 +28,9 @@ function abreOuFechaResposta(elemento){
 }
 
 function removeBotaoAtivo(){
-	const button = document.querySelectorAll('[data-tab-button]');	
-	for(let i = 0; i < button.lenght; i++){
-		button[i].classList.remove('shows__tabs__button--is-active');
+	const buttons = document.querySelectorAll('[data-tab-button]');	
+	for(let i = 0; i < buttons.lenght; i++){
+		buttons[i].classList.remove('shows__tabs__button--is-active');
 	}
 }
 
